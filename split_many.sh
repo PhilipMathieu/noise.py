6c6a304089eb9443c44e8a3ed2c5f2ca68ee0eca
PhilipMathieu <philip_mathieu@brown.edu>
2016-09-08 17:10:35 -0400

    Batch conversion (useful to reference)

---
+++ split_many.sh
@@ -0,0 +1,12 @@
+# ./uvrms.py extdata/splits/tsplits/ -t1h -f10MHz -d -sD --tsplit extdata/t1h10M.npz
+# ./uvrms.py extdata/splits/tsplits/ -t1h -f5MHz -d -sD --tsplit extdata/t1h5M.npz
+# ./uvrms.py extdata/splits/tsplits/ -t1h -f1MHz -d -sD --tsplit extdata/t1h1M.npz
+# ./uvrms.py extdata/splits/tsplits/ -t20min -f10MHz -d -sD --tsplit extdata/t20min10M.npz
+# ./uvrms.py extdata/splits/tsplits/ -t20min -f5MHz -d -sD --tsplit extdata/t20min5M.npz
+# ./uvrms.py extdata/splits/tsplits/ -t20min -f1MHz -d -sD --tsplit extdata/t20min1M.npz
+./uvrms.py extdata/splits/fsplits/ -t1h -f10MHz -d -sD extdata/f1h10M.npz
+./uvrms.py extdata/splits/fsplits/ -t1h -f5MHz -d -sD extdata/f1h5M.npz
+./uvrms.py extdata/splits/fsplits/ -t1h -f1MHz -d -sD extdata/f1h1M.npz
+./uvrms.py extdata/splits/fsplits/ -t20min -f10MHz -d -sD extdata/f20min10M.npz
+./uvrms.py extdata/splits/fsplits/ -t20min -f5MHz -d -sD extdata/f20min5M.npz
+./uvrms.py extdata/splits/fsplits/ -t20min -f1MHz -d -sD extdata/f20min1M.npz
